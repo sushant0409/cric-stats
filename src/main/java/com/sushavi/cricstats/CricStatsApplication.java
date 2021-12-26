2ebc4c862d37f279484ce70948383eb812986a57
Sushant <ersushantkm49@gmail.com>
2021-12-26 22:14:03 +0000

    first draft

---
+++ src/main/java/com/sushavi/cricstats/CricStatsApplication.java
@@ -0,0 +1,13 @@
+package com.sushavi.cricstats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CricStatsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CricStatsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sushavi/cricstats/CricStatsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sushavi.cricstats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CricStatsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
